76840d6f206da4344489a0a18d051d20245bedc0
Thiago <thiagomfsup@gmail.com>
2017-06-04 16:25:28 +0200

    Refactoring StartProcessor class

--- src/main/java/com/example/processor/StartProcessor.java
+++ src/main/java/com/example/processor/StartProcessor.java
@@ -1,16 +1,25 @@
 package com.example.processor;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
 
+/**
+ * A {@link Processor} implementation that capitalize a header, if found.
+ *
+ */
 public class StartProcessor implements Processor {
 
-	private static final String FOO_HEADER = "foo";
+	public static final String FOO_HEADER = "foo";
 	
+	/* (non-Javadoc)
+	 * @see org.apache.camel.Processor#process(org.apache.camel.Exchange)
+	 */
 	public void process(Exchange exchange) throws Exception {
-		String fooHeader = exchange.getIn().getHeader(FOO_HEADER, String.class);
+		final Message in = exchange.getIn();
+		final String fooHeader = in.getHeader(FOO_HEADER, String.class);
 		
 		// http://camel.apache.org/using-getin-or-getout-methods-on-exchange.html
-		exchange.getIn().setHeader(FOO_HEADER, fooHeader.toUpperCase());
+		in.setHeader(FOO_HEADER, (fooHeader == null ? "" : fooHeader).toUpperCase());
 	}	
 }
