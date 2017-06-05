022e3a8828f30a51e24ee52cc70da60a59df7548
Thiago <thiagomfsup@gmail.com>
2017-06-05 08:39:37 +0200

    Improving testing

--- src/test/java/com/example/test/FilterTest.java
+++ src/test/java/com/example/test/FilterTest.java
@@ -4,11 +4,14 @@ import org.apache.camel.EndpointInject;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.processor.StartProcessor;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { 
@@ -19,21 +22,40 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @DirtiesContext
 public class FilterTest {
 
+	private static final String LOWERCASE_MOCK_VALUE = "bar";
+
 	@EndpointInject(uri = "mock:result")
 	protected MockEndpoint resultEndpoint;
 
 	@Produce(uri = "direct:testStart")
 	protected ProducerTemplate template;
+	
+	@Before
+	public void setUp() {
+		resultEndpoint.reset();
+	}
 
 	@Test
-	public void test() throws Exception {
-		String expectedBody = "<matched/>";
+	public void testResponse() throws InterruptedException {
+		resultEndpoint.expectedBodiesReceived((Object) null);
+		resultEndpoint.expectedHeaderReceived(StartProcessor.FOO_HEADER,
+				LOWERCASE_MOCK_VALUE.toUpperCase());
 
-		resultEndpoint.expectedBodiesReceived(expectedBody);
-
-		template.sendBodyAndHeader(expectedBody, "foo", "bar");
+		template.sendBodyAndHeader(null, StartProcessor.FOO_HEADER,
+				LOWERCASE_MOCK_VALUE);
 
 		resultEndpoint.assertIsSatisfied();
 	}
 
+	@Test
+	public void testNotSendExpectedHeader() throws InterruptedException {
+		resultEndpoint.expectedBodiesReceived((Object) null);
+		resultEndpoint.expectedHeaderReceived(StartProcessor.FOO_HEADER, "");
+
+		template.sendBodyAndHeader(null, "UnexpectedHeader",
+				LOWERCASE_MOCK_VALUE);
+
+		resultEndpoint.assertIsSatisfied();
+	}
+	
 }
